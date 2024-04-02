21255b0bfd8c0f7f98d892cd5ac8d8d44f2fb1bf
Belen Caporale <caporalebelen14@gmail.com>
2024-04-02 11:23:43 -0300

    cambios

--- src/components/screens/HomeScreen.jsx
+++ src/components/screens/HomeScreen.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { FlatList, Image, StyleSheet, View, Text } from "react-native";
 import { usePokemonPaginate } from "../../hooks/usePokemonPaginate";
+
 
 export default function HomeScreen() {
   const { pokemonList, loadPokemons } = usePokemonPaginate();
+  
 
   return (
     <>
       <Image source={require("../../assets/poke.png")} 
       style={ styles.bgPokeball}
       />
+      {
+        pokemonList.length > 0 && (
+          <View style={styles.containerList}>
+            <FlatList 
+              data={pokemonList}
+              numColumns={2}
+              keyExtractor={(pokemon)=> String(pokemon.id) }
+              renderItem={({ item }) => <Text>{item.name}</Text> }
+              ListHeaderComponent={<Text style={styles.title}>Pokedex</Text>}
+            />
+          </View>
+        )
+      }
     </>
   );
 }
@@ -17,8 +32,21 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   bgPokeball: {
-    width: 100,
-    height: 100,
+    width: 300,
+    height: 300,
+    position: "absolute",
+    top: -130,
+    right: -130,
+    marginHorizontal: 20,
+    opacity: 0.2,
+  },
+  containerList: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginHorizontal: 20,
   }
 })
 
